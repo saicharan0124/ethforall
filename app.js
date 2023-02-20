9270a705e03a3970ab69e7dae9a3042f7f31eef6
Saicharan <81006148+saicharan0124@users.noreply.github.com>
2023-02-20 18:50:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ dotenv.config();
 connectDB();
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json()); // to accept json data
 
 // app.get("/", (req, res) => {
